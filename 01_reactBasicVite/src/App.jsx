345277c95f400e0b7c0a38a970ed1f9b877671c9
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-02-02 07:55:05 +0000

    react flow

--- 01_reactBasicVite/src/App.jsx
+++ 01_reactBasicVite/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Chai from "./chai"
 
 function App() {
  
 
   return (
-   <h1>Chai Aur React using Vite</h1>
+    <>
+      <Chai/>  
+      <h1>Hi this is hitesh</h1>
+    </>
+// ab technically hum yha pe ek hi element return krskte hai agar ek se zyada krenge to error aayega to isko bypass krne ke liye hum ek <> </> elementy ke andar hi saare elemnts daal dete hai. this emply elemenet is called Fragment. 
   )
 }
 
