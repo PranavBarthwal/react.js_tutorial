d1b41fe1f43e1d267d17ed891f30f7a3391e818c
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-02-02 09:32:18 +0000

    own custom react

--- 03_customReact&JSX/customreact.js
+++ 03_customReact&JSX/customreact.js
@@ -1,1 +1,25 @@
+function customRender(reactElement, container){
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+
+}
+
+
+const reactElement = {
+    type : 'a',
+    props : {
+        href : 'https://github.com/Pranavbarthwal',
+        target : '_blank'
+    },
+    children : 'Pranav Barthwal on GitHub'
+}
+
+
 const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
