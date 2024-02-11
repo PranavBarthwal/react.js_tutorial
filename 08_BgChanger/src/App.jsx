d6eb02e68c95e1c1b0e12cb6f5eaae5c16315fc2
PranavBarthwal <pbarthwal90@gmail.com>
2024-02-11 16:05:30 +0530

    BG changer

--- 08_BgChanger/src/App.jsx
+++ 08_BgChanger/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => setColor("red")}
+          onClick={() => setColor("red")}      // NOTE : in the end
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
@@ -35,3 +35,16 @@ function App() {
 }
 
 export default App
+
+
+
+// NOTE :
+
+// why cant we directly use onClick={setColor("red")} insted of passing a callback in onclick?
+
+// The issue with using onClick={setColor("red")} is that it will immediately invoke the setColor function
+// when the component renders. This is because the onClick prop expects a function to be passed to it, 
+// but you're immediately invoking the function by including the parentheses ().
+
+// To solve this issue, you can wrap the function call in another function, like an arrow function. 
+// This way, the function will only be invoked when the button is clicked, not when the component renders.
