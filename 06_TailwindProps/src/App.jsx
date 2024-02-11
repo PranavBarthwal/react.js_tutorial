9674fa336ff2e1facbacb8676645c9f6e09c46b3
PranavBarthwal <pbarthwal90@gmail.com>
2024-02-11 15:14:52 +0530

    props and components

--- 06_TailwindProps/src/App.jsx
+++ 06_TailwindProps/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Card from '../components/card'
+import Card from './components/card'
 
 function App() {
 
@@ -17,7 +17,7 @@ function App() {
   return (
     <>
       <h1 className='bg-red-400 text-yellow p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card username="chai aur code"  btnText="click" someObj={myObj} someArr={newArr}/>
+      <Card username="chai aur code"  btnText="click" someObj={myObj} someArr={newArr}/>         // ismein username, btntext, someObj, someArr are the props. array and objects ko directly pass ni krte use bahar variable men store krate h then us variable ko prop mein use krte hai
       <Card username="hitesh"/>
     </>
   )
