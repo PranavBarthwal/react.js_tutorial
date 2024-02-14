37148b35cdd0e5aa00a1ff2cf308f06ae78247a8
PranavBarthwal <pbarthwal90@gmail.com>
2024-02-14 21:29:22 +0530

    nput box

--- 10_CurrencyConverterAndCustomHooks/components/InputBox.jsx
+++ 10_CurrencyConverterAndCustomHooks/components/InputBox.jsx
@@ -3,22 +3,31 @@ import React from 'react'
 
 function InputBox({
     label,
-    
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
     className = "",
 }) {
    
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${username} `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
             <div className="w-1/2">
                 <label  className="text-black/40 mb-2 inline-block">
-                    label
+                    {label}
                 </label>
                 <input
                     
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
