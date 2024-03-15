6b1dc03edf21b3267151b76410c336f7c0d189da
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-03-15 11:45:36 +0000

    add context

--- 14_ToDoListUsingContextAPI&LocalStorage/src/contexts/TodoContexts.js
+++ 14_ToDoListUsingContextAPI&LocalStorage/src/contexts/TodoContexts.js
@@ -1,3 +1,19 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext();
+export const TodoContext = createContext({
+    todos : [{
+        id: 1,
+        todo: "Todo msg",
+        completed: false,
+    }],
+    addTodo: (todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+    updatedTodo: (id, todo) => {},    
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
