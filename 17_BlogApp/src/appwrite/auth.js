4282549bce3d4254d779680fc1308bde927c7a1e
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-03-26 18:04:35 +0000

    appwrite auth service

--- 17_BlogApp/src/appwrite/auth.js
+++ 17_BlogApp/src/appwrite/auth.js
@@ -13,8 +13,43 @@ export class AuthService{
         this.account = new Account(this.client);    
     }
 
-    
+    async createAccount({email, password, name}){
+        try{
+            const UserAccount = await this.account.create(ID.unique(), email, password, name);
+            if(UserAccount){
+                return this.login({email, password});
+            }else{
+                return UserAccount;
+            }
+        }catch(error){
+            throw error;
+        }
+    }
 
+    async login({email, password}){
+        try {
+           return await this.accountcreateEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 const authService = new AuthService();
