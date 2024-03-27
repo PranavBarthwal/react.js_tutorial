f2a13e8d3bef2607b58cffb4f45ed2b365c400df
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-03-27 21:50:34 +0000

    add button

---
+++ 17_BlogApp/src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button(
+    children,
+    type
+) {
+    return (
+        <button>
+            {children}
+        </button>
+    )
+}
+
+export default Button
